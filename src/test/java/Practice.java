f98608ca8babb21ab7d19f611879b2781e616af5
nwankhede <nwankhede@L-INE-DZRCQG3.ardianet.net>
2024-01-24 11:07:49 +0530

    Commiting first code

---
+++ src/test/java/Practice.java
@@ -0,0 +1,20 @@
+
+public class Practice {
+
+public static void main(String[] args) {
+		
+		int a=1;
+
+		for (int i=0; i<=4; i++)
+		{
+			for(int j=i+1; j<=4; j++)
+			{
+				System.out.print(a);
+				System.out.print("\t");
+				a++;
+			}
+			System.out.println();
+		}
+	}
+
+}
